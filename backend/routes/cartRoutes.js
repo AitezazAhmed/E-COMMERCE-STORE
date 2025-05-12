1a944ec8a43e3285e70a6072c0e0708acd5a66e2
AitezazAhmed <stoneghold4103@gmail.com>
2025-05-13 01:45:51 +0500

    cartRoutes.js

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import addToCart from "../controllers/cart.controller"
-import restrictToLoggedinUserOnly from "../middleware/auth.middleware"
+import {addToCart,getCartProducts,removeAllFromCart,updateQuantity} from "../controllers/cart.controller.js"
+import {restrictToLoggedinUserOnly} from "../middleware/auth.middleware.js"
 const router =express.Router();
 router.get("/",restrictToLoggedinUserOnly,getCartProducts)
 router.post("/" ,restrictToLoggedinUserOnly, addToCart)
