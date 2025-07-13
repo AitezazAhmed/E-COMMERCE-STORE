f53f697ba0b8ba25efe6700f48a1a2278a47241e
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-14 03:45:39 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,10 @@ app.use(express.urlencoded({ limit: '100mb', extended: true }));
 const PORT=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:5173", // for local development
-  "https://e-commerce-store-1-qzjs.onrender.com" // your deployed frontend
-];
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: ['http://localhost:5173', 'https://e-commerce-store-5c9i.vercel.app/'],
   credentials: true
 }));
-
 
 
 app.use("/api/auth",authRoutes)
