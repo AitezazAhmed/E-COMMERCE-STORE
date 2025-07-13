c45e8cd304cf3c244ec06d39752b411e8d25dd15
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-14 01:44:36 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from 'cors';
-import path from "path";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/productRoute.js"
 import cartRoutes from "./routes/cartRoutes.js"
@@ -9,10 +8,8 @@ import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser";
-
 dotenv.config()
 const app= express()
-const __dirname = path.resolve();
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 const PORT=process.env.PORT || 5000
@@ -27,13 +24,6 @@ app.use("/api/products",productRoutes)
 app.use("/api/cartRoute",cartRoutes)
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 app.listen(PORT,()=>{
     console.log("server is running",PORT)
     connectDB()
