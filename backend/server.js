43b814a20bed79af1398e87e122a21c0482fc9c3
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-14 01:03:21 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from 'cors';
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/productRoute.js"
 import cartRoutes from "./routes/cartRoutes.js"
@@ -12,6 +12,7 @@ import cookieParser from "cookie-parser";
 
 dotenv.config()
 const app= express()
+const __dirname = path.resolve();
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 const PORT=process.env.PORT || 5000
@@ -26,6 +27,13 @@ app.use("/api/products",productRoutes)
 app.use("/api/cartRoute",cartRoutes)
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 app.listen(PORT,()=>{
     console.log("server is running",PORT)
     connectDB()
