090b4db75dc7e4cf78212a336618d26f97b3ba00
AitezazAhmed <stoneghold4103@gmail.com>
2025-07-19 13:49:16 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   withCredentials: true, // allows cookies (for JWT)
 });
-
-export default instance;
